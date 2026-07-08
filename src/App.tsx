d620c4aa538f0096545fcc34011ebdbb2026e026
Natthapong Thongsukha <95947489+jajay456@users.noreply.github.com>
2026-07-08 17:28:37 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
           setAuthState('unauthorized')
         }
       })
-      .catch((e) => {
+      .catch(() => {
         if (!cancelled) setAuthState('error')
       })
 
